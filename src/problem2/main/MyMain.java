4bba145c67f0f4bd0d35de5c50e38addaf6fffa0
mukund agrawal <mukund.agrawal_cs18@gla.ac.in>
2020-03-28 19:14:48 +0530

    problem2 completed

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,51 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+import problem1.mybst.MyBinarySearchTree;
+
+
+
 // executable class
+
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
 public class MyMain {
+
+    public static void main(String[] args) {
+
+        MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<>();
+
+        myBinarySearchTree.add(80);
+
+        myBinarySearchTree.add(54);
+
+        myBinarySearchTree.add(588);
+
+        myBinarySearchTree.add(46);
+
+        myBinarySearchTree.add(87);
+
+        System.out.println("Post order traversing");
+
+        myBinarySearchTree.traversePostOrder(myBinarySearchTree.getRoot());
+
+        System.out.println();
+
+        System.out.println("Pre order traversing");
+
+        myBinarySearchTree.traversePreOrder(myBinarySearchTree.getRoot());
+
+        // here first element in pre order same as last element in post order//
+
+        // here middle Node/element in preOrder and PostOrder traversing not same so false //
+
+
+
+    }
+
+
+
+
+
 }
+
